9e6e2a669955bfb90dac5b8c0d7d8bf935218538
Anass El Omari <anas.iramol@gmail.com>
2024-03-22 04:53:42 -0700

    fix task 1- install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,23 +1,23 @@
 # Install Flask package 2.1.0
 
 class { 'python':
-  version => '3.8.10'
+  version => '3',
 }
 
 package { 'python3-pip':
   ensure   => 'present',
   provider => 'pip3',
-  require  => Class['python']
+  require  => Class['python'],
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip']
+  require  => Package['python3-pip'],
 }
 
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['python3-pip']
+  require  => Package['python3-pip'],
 }
