13cd37cf6010233d2c2a50478213b1a989c4d91b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-26 20:49:00 +0000

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,9 +12,6 @@ file { '/var/www/html/index.html':
 
 exec { 'append_redirect_me':
   command     => "/usr/bin/sed -i '/^}$/i \\ \n\tlocation /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}' /etc/nginx/sites-available/default",
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => Package['nginx'],
 }
 
 service { 'nginx':
