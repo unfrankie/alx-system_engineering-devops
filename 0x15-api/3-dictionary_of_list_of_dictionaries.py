5cc621a43a511dd2090e65ff198cf9129a64b841
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-22 18:57:19 +0100

    Create 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Exports data in the JSON format."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users'
+    users = requests.get(url).json()
+
+    all_tasks = {}
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+
+        todo_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+        todos = requests.get(todo_url).json()
+
+        user_tasks = []
+        for todo in todos:
+            task = {
+                "username": username,
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+            user_tasks.append(task)
+
+        all_tasks[user_id] = user_tasks
+
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(all_tasks, json_file)
