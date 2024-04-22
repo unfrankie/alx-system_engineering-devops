883bc62f40ff4b6988818da799ebed66c60ca448
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-22 18:54:51 +0100

    Create 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+def gather_data_from_api(employee_id):
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+    response = requests.get(url)
+    todos = response.json()
+
+    if not todos:
+        print("No data found for employee ID: {}".format(employee_id))
+        return
+
+    total_tasks = len(todos)
+    completed_tasks = [todo for todo in todos if todo['completed']]
+
+    employee_name = todos[0]['userId']
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed_tasks), total_tasks))
+    for task in completed_tasks:
+        print("\t{}".format(task['title']))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    gather_data_from_api(employee_id)
