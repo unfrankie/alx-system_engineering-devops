e023b86bd3792750d6d01cc94f99a76cd95d4c98
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-07 16:31:50 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit
+Queries the Reddit API and prints the titles
+of the first 10 hot posts for a given subreddit
 """
 import requests
 
