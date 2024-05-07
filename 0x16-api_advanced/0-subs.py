ebe09249fc9a92999d67133fb694768de135f2be
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-07 16:40:11 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,9 @@ def number_of_subscribers(subreddit):
     Retrieves the number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {
+        'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
+    }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
