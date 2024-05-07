d5134efe2d889f90d36c1fa0b8d32a3b7dcb6087
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-07 16:32:54 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit
+Queries the Reddit API and returns the number
+of subscribers for a given subreddit
 """
 import requests
 
