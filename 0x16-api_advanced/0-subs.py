314c10e64c77884e5b9643130eb8e14d368bd472
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-07 16:43:04 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,8 +14,8 @@ def number_of_subscribers(subreddit):
     headers = {
         'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
