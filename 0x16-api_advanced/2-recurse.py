a6926e8c96bcd77895f05e0e2f8db78fa13d95a9
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-07 16:24:48 +0100

    Create 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Recursively queries the Reddit API and returns a list containing titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Recursively retrieves the titles of all hot articles for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 100, 'after': hot_list[-1] if hot_list else None}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            hot_list.append(post['data']['title'])
+        if data['data']['after'] is not None:
+            recurse(subreddit, hot_list)
+        return hot_list
+    return None
