2479aff95ed28e07af81d496118f836b06014485
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-08 19:30:48 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
 # Puppet manifest to fix Apache returning a 500 error
 
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => 'running',
-}
-
-# Fix the issue causing Apache to return a 500 error
-exec { 'fix-apache-error':
-  command     => 'sed -i "s/SomeWrongSetting/CorrectSetting/g" /etc/apache2/apache2.conf',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => Service['apache2'],
+exec { 'fix wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
