6da250e8279f18e9f6a40ad3349254b04474edcf
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-16 16:03:48 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # Sky is the limit, let's bring that limit higher
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/Document Length: 201/Document Length: 612/" /etc/nginx/sites-available/default',
-  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
-  onlyif  => '/usr/bin/test -f /etc/nginx/sites-available/default',
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
